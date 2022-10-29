b40c40b701df00b87135c3459c2f9d43fd592175
Tran Quang <quang.tran@realstake.io>
2022-10-29 16:22:33 +0700

    Fix get ctx string check nil

--- libs/utils/ctx.go
+++ libs/utils/ctx.go
@@ -7,7 +7,7 @@ import (
 
 func GetCtxStr(ctx context.Context, key string) string {
 	v := ctx.Value(key)
-	if reflect.TypeOf(v).Kind() != reflect.String {
+	if v == nil || reflect.TypeOf(v).Kind() != reflect.String {
 		return ""
 	}
 	return v.(string)
